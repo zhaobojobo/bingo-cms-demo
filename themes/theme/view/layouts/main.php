6248eae0b16b30f9a3d51c25eeb2ec93ba0c02ef
zb <94091709@qq.com>
2026-03-15 12:43:08 +0800

    添加hook

--- themes/theme/view/layouts/main.php
+++ themes/theme/view/layouts/main.php
@@ -13,8 +13,10 @@ $site = setting('site');
 <!-- Html Head ------><?= $this->fetch('partials/head', ['site' => $site]); ?><!-- /Html head -->
 <body>
 <!-- Page Header ----><?= $this->fetch('partials/header', ['site' => $site]); ?><!-- /Page Header -->
+<?= $this->section('custom_header') ?>
 <!-- Page Content ---><?= $this->section('content') ?><!-- /Page Content -->
 <!-- Page Footer ----><?= $this->fetch('partials/footer', ['site' => $site]); ?><!-- /Page Footer -->
 <!-- Page Scripts ---><?= $this->fetch('partials/scripts'); ?><!-- /Page Scripts -->
+<?= $this->section('custom_footer') ?>
 </body>
 </html>
